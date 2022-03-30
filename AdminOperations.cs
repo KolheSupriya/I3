31f1713ce6ba5f299c2621cfb0069449d9dac10a
Supriya Nivrutti Kolhe <supriyakol@cybage.com>
2022-03-30 14:10:05 +0530

    first commit

---
+++ AdminOperations.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    class AdminOperations
+    {
+        static int user_choice;
+        public static void AdminMenu()
+        {
+            var ctx = new MyDebContext();
+            //Console.WriteLine("inside usermenu");
+            while (user_choice == 0)
+            {
+                Console.WriteLine("----------------------------------------\nWhat operation do you wish to perform?\n1. Add User\n2. Add Role\n0. Logout\n----------------------------------------");
+                user_choice = Convert.ToInt32(Console.ReadLine());
+                switch (user_choice)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("----------------------------------------\nEnter following details:");
+                            Console.WriteLine("Role_ID ,User_Name,First_Name,Last_Name,Password,City,Address,Zip_Code,Email ");
+                            int roleId = Convert.ToInt32(Console.ReadLine());
+                            string User_Name = Console.ReadLine();
+                            string First_Name = Console.ReadLine();
+                            string Last_Name = Console.ReadLine();
+                            string Password = Console.ReadLine();
+                            //string DOB = Convert.ToInt32(Console.ReadLine());
+                            string City = Console.ReadLine();
+                            string Address = Console.ReadLine();
+                            int Zip_Code = Convert.ToInt32(Console.ReadLine());
+                            string Email = Console.ReadLine();
+                            int productIncharge = Convert.ToInt32(Console.ReadLine());
+
+                            USERS data = new USERS() {Product_Access=productIncharge, Role_ID = roleId, User_Name = User_Name, First_Name = First_Name, Last_Name = Last_Name, Password = Password, City = City, Address = Address, Zip_Code = Zip_Code, Email = Email };
+                            ctx.USERS.Add(data);
+                            if (ctx.SaveChanges() > 0)
+                            {
+                                Console.WriteLine("Result-> User Added");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Result-> User not added. Please confirm details");
+                            }
+
+                            break;
+                        }
+                    case 2:
+                        {
+
+
+
+                            break;
+
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Please enter correct choice");
+                            break;
+                        }
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace InventoryManagementSystem
+{
+    class Program
+    {
+        static username="";
+        static int choice;
+        //static int logout=0;
+        static void Main(string[] args)
+        {
+            //addRole();
+            while (choice==0)
+            {
+
+
+                Console.WriteLine("Please login. Choices are as follows\n1. Admin \n2. User\n0. Exit\n----------------------------------------");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            AdminOperations.AdminMenu();
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            UserOperations.UserMenu();
+                                                       
+                            //logout = 0;
+                            continue;
+                        }
+                    case 3:
+                        {
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Wrong choice entered. Please try again. ");
+                            break;
+                        }
+                }
+            }
+        }
+        //public static void addRole()
+        //{
+        //    var ctx = new MyDebContext();
+        //    ROLE adminObj = new ROLE() {Role_Name="admin" };
+        //    ROLE userObj = new ROLE() {Role_Name="user" };
+        //    ctx.ROLES.Add(adminObj);
+        //    ctx.ROLES.Add(userObj);
+        //}
+    }
+    class MyDebContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Server =.; Database = AngularReact; Trusted_Connection = True; ");
+            //optionsBuilder.UseSqlServer(@"Server =SUPRIYAKOL-VD\SQL2017; Database=InventoryMgtSystem; User ID=sa; Password=cybage@123456;");
+            optionsBuilder.UseSqlServer(@"Server =SUPRIYAKOL-VD\SQL2017; Database=IMS; User ID=sa; Password=cybage@123456;");
+        }
+        public virtual DbSet<USERS> USERS { get; set; }
+        //public virtual DbSet<INVOICE> INVOICE_DATA { get; set; }
+        public virtual DbSet<PRODUCT_CATEGORIES> PRODUCT_CATEGORIES { get; set; }
+        //public virtual DbSet<STORAGE> STORAGES { get; set; }
+        public virtual DbSet<PRODUCTS> PRODUCTS { get; set; }
+        public virtual DbSet<ROLE> ROLES { get; }
+        //public virtual DbSet<SHIPPING> SHIPPING { get; set; }
+
+    }
+
+
+}
